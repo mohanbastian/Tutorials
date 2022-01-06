5be51aa0c662ac643fba6b00c98181c9947fdc16
George Mohan Bastiampillai <mohan.bastian@gmail.com>
2022-01-06 14:33:08 +0800

    AddTask toggle and code refactoring

--- vue-crash-2022/src/App.vue
+++ vue-crash-2022/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" :showAddTask="showAddTask"/>
+    <div v-show="showAddTask">
+    <AddTask @add-task="addTask" />
+    </div>
     <Tasks
       @toggle-reminder="toggleReminder"
       @delete-task="deleteTask"
@@ -12,19 +15,25 @@
 <script>
 import Header from "./components/Header.vue";
 import Tasks from "./components/Tasks.vue";
+import AddTask from "./components/AddTask.vue";
 
 export default {
   name: "App",
   components: {
     Header,
     Tasks,
+    AddTask
   },
   data() {
     return {
       tasks: [],
+      showAddTask: false
     };
   },
   methods: {
+    toggleAddTask(){
+      this.showAddTask = !this.showAddTask
+    },
     deleteTask(id) {
       this.tasks = this.tasks.filter((task) => task.id !== id);
     },
@@ -32,6 +41,9 @@ export default {
       this.tasks = this.tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
       );
+    },
+    addTask(task){
+this.tasks = [...this.tasks, task]
     },
   },
   created() {
--- vue-crash-2022/src/components/Header.vue
+++ vue-crash-2022/src/components/Header.vue
@@ -1,29 +1,33 @@
 <template>
-    <header>
-        <h1>{{ title }}</h1>
-        <Button text="Add Task" color="Green" />
-    </header>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+      @btn-click="$emit('toggle-add-task')"
+      :text="showAddTask ? 'Close' : 'Add Task'"
+      :color="showAddTask ? 'Red' : 'Green'"
+    />
+  </header>
 </template>
 
 <script>
-import Button from './Button.vue'
-export default{
-    name:'Header',
-    props:{
-        title: String,
-    },
-    components:{
-        Button
-    }
-    }
-
+import Button from "./Button.vue";
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {
+    Button,
+  },
+};
 </script>
 
 <style scoped>
-header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 20px;
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
 }
 </style>
